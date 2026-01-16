fc1a609f662b38bf1e43db85fa39847e50012edf
Tsvetan Todorov Todorov <111016955+TsvetanTodorovTodorov@users.noreply.github.com>
2026-01-16 08:56:09 +0200

    Add files via upload

---
+++ 02TownPopulation.js
@@ -0,0 +1,32 @@
+function townPopulation(data) {
+  let towns = {};
+
+  for (let townData of data) {
+    let [name, population] = townData.split(" <-> ");
+    population = Number(population);
+
+    if (towns.hasOwnProperty(name)) {
+      towns[name] += population;
+    } else {
+      towns[name] = population;
+    }
+  }
+
+  for (const [name, population] of Object.entries(towns)) {
+    console.log(`${name} : ${population}`);
+  }
+}
+townPopulation([
+  "Sofia <-> 1200000",
+  "Montana <-> 20000",
+  "New York <-> 10000000",
+  "Washington <-> 2345000",
+  "Las Vegas <-> 1000000",
+]);
+townPopulation([
+  "Istanbul <-> 100000",
+  "Honk Kong <-> 2100004",
+  "Jerusalem <-> 2352344",
+  "Mexico City <-> 23401925",
+  "Istanbul <-> 1000",
+]);
